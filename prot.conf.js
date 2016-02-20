32657b5d845635ee030f61e11ec26e711018d8b6
Robert Messerle <rmesserle@google.com>
2016-02-19 16:35:24 -0800

    switching back to firefox

--- prot.conf.js
+++ prot.conf.js
@@ -13,7 +13,7 @@ exports.config = {
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
   capabilities: {
-    'browserName': 'chrome',
+    'browserName': 'firefox',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'name': 'Material 2 Protractor Tests'
   }
