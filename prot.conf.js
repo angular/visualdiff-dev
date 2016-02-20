a30b01b0732b12d6a1348db0f39feae171f332dc
Robert Messerle <rmesserle@google.com>
2016-02-19 16:16:57 -0800

    testing

--- prot.conf.js
+++ prot.conf.js
@@ -1,5 +1,12 @@
 require('./scripts/sauce/sauce_config.js');
 
+console.log({
+  tunnelId: process.env.TRAVIS_JOB_NUMBER,
+  build: process.env.TRAVIS_JOB_NUMBER,
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY
+            });
+
 exports.config = {
   specs: [ './e2e/**/*.spec.js' ],
   baseUrl: 'http://localhost:3333',
@@ -8,7 +15,6 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_JOB_NUMBER,
     'name': 'Material 2 Protractor Tests'
   }
 };
