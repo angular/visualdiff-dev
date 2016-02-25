c2363e0ed6337d0977ea84d87e1dc3468c93b886
Robert Messerle <rmesserle@google.com>
2016-02-25 15:45:41 -0800

    using relative paths

--- e2e/index/index.spec.js
+++ e2e/index/index.spec.js
@@ -15,8 +15,8 @@ describe('hello, protractor', function () {
 });
 
 function screenshot(id) {
-  var screenshotPath = path.resolve(__dirname, '../../screenshots', id + '.screenshot.png');
-  var tempPath = path.resolve(__dirname, '../../screenshots', id + '.temp.png');
+  var screenshotPath = path.resolve('./screenshots', id + '.screenshot.png');
+  var tempPath = path.resolve('./screenshots', id + '.temp.png');
 
   browser.takeScreenshot().then(handleNewScreenshot);
 
@@ -41,23 +41,12 @@ function screenshot(id) {
   }
 
   function compareImages() {
-    //var gold = mapnik.Image.open(screenshotPath);
-    //var temp = mapnik.Image.open(tempPath);
-    //var changed = gold.compare(temp);
-    //overwriteExistingScreenshot();
-    //if (gold.compare(temp)) {
-    //  throw new Error('screenshot "' + id + '" has changed.');
-    //}
-    var gold = fs.readFileSync(screenshotPath);
-    var temp = fs.readFileSync(tempPath);
-    resemble(gold)
-        .compareTo(temp)
-        .onComplete(function (data) {
-          var changed = Number(data.misMathPercentage) > 0;
-          overwriteExistingScreenshot();
-          if (changed) {
-            throw new Error('screenshot "' + id + '" has changed.');
-          }
-        });
+    var gold = mapnik.Image.open(screenshotPath);
+    var temp = mapnik.Image.open(tempPath);
+    var changed = gold.compare(temp);
+    overwriteExistingScreenshot();
+    if (gold.compare(temp)) {
+      throw new Error('screenshot "' + id + '" has changed.');
+    }
   }
 }
