fbffe29320be6678eb36c8799bc5f2119187fee0
Robert Messerle <rmesserle@google.com>
2016-02-25 10:46:24 -0800

    updated screenshot, modified comparison logic

--- e2e/index/index.spec.js
+++ e2e/index/index.spec.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var resemble = require('node-resemble-js');
+var path = require('path');
 
 describe('hello, protractor', function () {
   describe('index', function () {
@@ -12,30 +13,36 @@ describe('hello, protractor', function () {
 });
 
 function screenshot(id) {
-  var path = './screenshots/' + id + '.screenshot.png';
+  var screenshotPath = path.resolve(__dirname, '../../screenshots', id + '.screenshot.png');
   browser.takeScreenshot().then(handleScreenshot);
 
   function handleScreenshot(png) {
+    console.log('handleScreenshot');
     compareToMaster(new Buffer(png, 'base64'));
   }
 
   function compareToMaster(screenshot) {
-    fs.readFile(path, function (err, gold) {
-      if (err) writeImage(screenshot, path);
+    console.log('compareToMaster', screenshot);
+    fs.readFile(screenshotPath, function (err, gold) {
+      console.log('comparing');
+      if (err) writeImage(screenshot, screenshotPath);
       else compareImages(screenshot, gold);
     });
   }
 
   function writeImage(image, path) {
+    console.log('writeImage', image, path);
     var stream = fs.createWriteStream(path);
     stream.write(image);
     stream.end();
   }
 
   function compareImages(screenshot, gold) {
+    console.log('compareImages', screenshot, gold);
     resemble(screenshot).compareTo(gold).onComplete(function (data) {
+      console.log(data);
+      writeImage(screenshot, screenshotPath);
       if (data.misMatchPercentage > 0) throw new Error('Screenshot "' + id + '" does not match.');
-      else writeImage(screenshot, path);
     });
   }
 }
