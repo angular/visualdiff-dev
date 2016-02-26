61088e438c3f05ae5dc2d3cf815dde4cf6300ac9
Robert Messerle <rmesserle@google.com>
2016-02-25 23:53:56 -0800

    screenshots are pulled from github rather than the repo

--- e2e/index/index.spec.js
+++ e2e/index/index.spec.js
@@ -1,7 +1,10 @@
+var https = require('https');
 var fs = require('fs');
 var path = require('path');
 var child_process  = require('child_process');
 var mapnik = require('mapnik');
+
+var SHA = process.env.TRAVIS_COMMIT;
 
 describe('hello, protractor', function () {
   describe('index', function () {
@@ -14,27 +17,26 @@ describe('hello, protractor', function () {
 });
 
 function screenshot(id) {
-  var screenshotPath = path.resolve(__dirname, '..', '..', 'screenshots', id + '.screenshot.png');
+  var screenshotPath = path.resolve('/tmp', id + '.screenshot.png');
+  var screenshotUrl = 'https://raw.githubusercontent.com/angular/visualdiff-dev/' + SHA + '/screenshots/' + id + '.screenshot.png';
   var goldScreenshot, newScreenshot;
 
   browser.takeScreenshot().then(handleNewScreenshot);
 
   function handleNewScreenshot(png) {
     newScreenshot = new Buffer(png, 'base64');
-    checkForExistingScreenshot();
+    getExistingScreenshot();
   }
 
-  function checkForExistingScreenshot() {
-    fs.access(screenshotPath, fs.F_OK, function (err) {
-      if (!err) {
-        fs.readFile(screenshotPath, function (err, data) {
-          goldScreenshot = new Buffer(data, 'base64');
-          compareImages();
-        });
-      } else {
-        overwriteExistingScreenshot();
-        throw new Error('screenshot "' + id + '" does not exist.');
-      }
+  function getExistingScreenshot() {
+    goldScreenshot = fs.createWriteStream(screenshotPath);
+    https.get(screenshotUrl, function(res) {
+      res.on('data', function (chunk) {
+        goldScreenshot.write(chunk);
+      });
+      res.on('finish', function () {
+        compareImages();
+      });
     });
   }
 
